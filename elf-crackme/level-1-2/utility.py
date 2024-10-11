f0be2407e5c9f1273819067c89c20016b81ea8bd
PowerHup <powerhup11@gmail.com>
2024-10-12 00:27:41 +0800

    Refactor interactive menu in utility.py
    The interactive menu in utility.py has been refactored to use a while loop instead of a single prompt. This allows the user to continue selecting actions until they choose to exit. The options to modify the ELF file, check the repaired content, and exit are now displayed in a loop. If an invalid option is entered, an error message is shown.

--- elf-crackme/level-1-2/utility.py
+++ elf-crackme/level-1-2/utility.py
@@ -47,18 +47,21 @@ if __name__ == "__main__":
     print("")
     print("We have modified the Address and Offset of a GOT table entry in the Section Header.")
     print("Please try to restore them. After restoring, execute to obtain the flag.")
-    print("Select the action you want to perform:")
-    print("1. Modify the ELF file")
-    print("2. Check the repaired content")
-    print("3. Exit")
-        
-    choice = input("Enter the option number: ")
-        
-    if choice == "1":
-        patch()
-    elif choice == "2":
-        check()
-    elif choice == "3":
-        print("The program has exited.")
-    else:
-        print("Invalid option, please enter again.")
+    
+    while True:
+        print("Select the action you want to perform:")
+        print("1. Modify the ELF file")
+        print("2. Check the repaired content")
+        print("3. Exit")
+            
+        choice = input("Enter the option number: ")
+            
+        if choice == "1":
+            patch()
+        elif choice == "2":
+            check()
+        elif choice == "3":
+            print("The program has exited.")
+            break
+        else:
+            print("Invalid option, please enter again.")
